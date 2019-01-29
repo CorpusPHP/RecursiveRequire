f3dd32afdd1c70d21f87acd77a3144def0f32465
Jesse Donat <donatj@gmail.com>
2019-01-29 15:12:31 -0600

    Adds basic phpunit test

---
+++ test/Corpus/RecursiveRequire/LoaderTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Corpus\RecursiveRequire;
+
+use PHPUnit\Framework\TestCase;
+
+class LoaderTest extends TestCase {
+
+	public function test__invoke() : void {
+		$loader = new Loader(__DIR__ . '/TestDir');
+
+		$out = $loader();
+		$this->assertEquals([
+			__DIR__ . '/TestDir/file1.php'         => true,
+			__DIR__ . '/TestDir/SubDir1/file2.php' => 'foo',
+		], $out);
+	}
+
+	public function test__invoke_empty() : void {
+		$loader = new Loader(__DIR__ . '/EmptyTestDir');
+
+		$this->assertSame([], $loader());
+	}
+}
