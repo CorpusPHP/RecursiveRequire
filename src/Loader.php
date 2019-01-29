f532ee793e739b938cc586ea6253731c447f2348
Jesse Donat <donatj@gmail.com>
2019-01-29 13:46:15 -0600

    Adds Rough Library

---
+++ src/Loader.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Corpus\RecursiveRequire;
+
+class Loader {
+
+	/**
+	 * @var string
+	 */
+	protected $path;
+
+	/**
+	 * @param string $path Root path
+	 */
+	public function __construct( string $path ) {
+		$this->path = rtrim($path, DIRECTORY_SEPARATOR);
+	}
+
+	/**
+	 * Trigger the require's
+	 *
+	 * @param string $regex
+	 * @return array The result as a map of filename to return value.
+	 */
+	public function __invoke( string $regex = "/\\.php$/" ) {
+		$output = [];
+
+		$dir   = new \RecursiveDirectoryIterator($this->path);
+		$ite   = new \RecursiveIteratorIterator($dir);
+		$files = new \RegexIterator($ite, $regex);
+
+		foreach( $files as $file ) {
+			$output[(string)$file] = require $file;
+		}
+
+		return $output;
+	}
+
+}
